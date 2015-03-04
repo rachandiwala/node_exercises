ef7834d649d4446e639e1c69a2d555b826d543ec
Raza Chandiwala <rachandiwala@gmail.com>
2015-03-03 20:50:08 -0500

    First two exer completed

---
+++ learnyounode/program.js
@@ -0,0 +1,32 @@
+//console.log("HELLO WORLD");
+
+//console.log(process.argv);
+var total = 0;
+
+for(var i =2; i < process.argv.length; i++) {
+
+   total += Number(process.argv[i]);
+}
+
+  console.log(total);
+
+
+
+//    Example
+// print process.argv
+/*
+
+process.argv.forEach(function(val, index, array) {
+  console.log(index + ': ' + val);
+});
+
+      Example 2:
+
+  for (var i = 2; i < process.argv.length; i++){
+    result += Number(process.argv[i]);
+}
+  console.log(result);
+
+
+
+*/
