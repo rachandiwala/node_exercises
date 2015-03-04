5bdea2a9779de5e1c421e6370d909081d28cba88
Raza Chandiwala <rachandiwala@gmail.com>
2015-03-04 18:35:28 -0500

    completed Filter LS

---
+++ learnyounode/filterLs.js
@@ -0,0 +1,43 @@
+/*
+var fs = require('fs')
+var dir = process.argv[2];
+
+fs.readdir(dir, function(err, files){
+  files.forEach()
+
+if (err)
+
+
+
+});
+
+*/
+
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function (err, list) {
+  list.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+      console.log(file)
+  })
+})
+
+
+
+
+
+
+/*    
+
+var fs = require('fs')
+    var file = process.argv[2]
+
+    fs.readFile(file, function (err, contents) {
+      // fs.readFile(file, 'utf8', callback) can also be used
+      var lines = contents.toString().split('\n').length - 1
+      console.log(lines)
+    })
+
+
+*/
